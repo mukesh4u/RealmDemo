a830af397b6a6b54482fc4b83d25eabfc05405c3
Mukesh <mukesh.yadav@affle.com>
2017-11-30 16:28:07 +0530

    -db curd opertaion and realm db browse on chrome

--- app/src/main/java/com/android/developer/solutions/realmdemo/activity/MainActivity.java
+++ app/src/main/java/com/android/developer/solutions/realmdemo/activity/MainActivity.java
@@ -15,9 +15,9 @@ import com.android.developer.solutions.realmdemo.model.Building;
 import com.android.developer.solutions.realmdemo.model.Plot;
 
 import java.util.Random;
-import java.util.UUID;
 
 import io.realm.Realm;
+import io.realm.RealmList;
 import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,14 +49,14 @@ public class MainActivity extends AppCompatActivity {
         String result ="";
         for (int i=0;i<plots.size(); i++) {
             result = result+ plots.get(0).toString();
-            Realm.getDefaultInstance().beginTransaction();
+            //Realm.getDefaultInstance().beginTransaction();
             //realm not supporting cascading
             //so for that we have to delete data like this
-            if(plots.get(0).getBuildingArrayList() != null) {
-                plots.get(0).getBuildingArrayList().deleteAllFromRealm();
-            }
-            plots.get(0).deleteFromRealm();
-            Realm.getDefaultInstance().commitTransaction();
+//            if(plots.get(0).getBuildingArrayList() != null) {
+//                plots.get(0).getBuildingArrayList().deleteAllFromRealm();
+//            }
+//            plots.get(0).deleteFromRealm();
+            //Realm.getDefaultInstance().commitTransaction();
         }
 
         Log.d("result is===",result);
@@ -67,27 +67,41 @@ public class MainActivity extends AppCompatActivity {
         try {
             Realm.init(this);
             Realm.getDefaultInstance().beginTransaction();  //open the database
+            long id =1;
+            if (Realm.getDefaultInstance().where(Plot.class) != null &&
+                    Realm.getDefaultInstance().where(Plot.class).max("id") != null ) {
+                id = Realm.getDefaultInstance().where(Plot.class).max("id").longValue()+1;
+            }
+
             //database operation
-            Plot obj = Realm.getDefaultInstance().createObject(Plot.class,new Random().nextInt()+50);  //this will create a
+            Plot obj = Realm.getDefaultInstance().createObject(Plot.class,id);  //this will create a
             //plot object which will be inserted in database
 
-            //obj.setId(1);
             obj.setTitle("plot1");
             obj.setNo_of_building(2);
+            RealmList<Building> buildings = new RealmList<>();
             for(int i=0;i<obj.getNo_of_building();i++) {
-                insertBuildingData(obj,i);
+                Building building = insertBuildingData(obj,i);
+                buildings.add(building);
             }
+            obj.setBuildingArrayList(buildings);
             Realm.getDefaultInstance().commitTransaction(); //close the database
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void insertBuildingData(Plot obj, int count) {
-        Building building = Realm.getDefaultInstance().createObject(Building.class,new Random().nextInt()+50);  //this will create a
+    private Building insertBuildingData(Plot obj, int count) {
+        long id =1;
+        if (Realm.getDefaultInstance().where(Building.class) != null &&
+                Realm.getDefaultInstance().where(Building.class).max("id") != null ) {
+           id = Realm.getDefaultInstance().where(Building.class).max("id").longValue()+1;
+        }
+        Building building = Realm.getDefaultInstance().createObject(Building.class,id);  //this will create a
         building.setPlot(obj);
         building.setNo_of_floors(3);
         building.setTitle("Building "+count);
+        return building;
     }
 
     @Override
--- app/src/main/java/com/android/developer/solutions/realmdemo/application/MyApplication.java
+++ app/src/main/java/com/android/developer/solutions/realmdemo/application/MyApplication.java
@@ -1,9 +1,13 @@
 package com.android.developer.solutions.realmdemo.application;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.Environment;
 
 import com.facebook.stetho.Stetho;
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import java.io.File;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -19,19 +23,40 @@ public class MyApplication extends Application {
 
 
         Realm.init(this);
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
-                        .build());
-        RealmConfiguration config = new RealmConfiguration.Builder()
-                .name("landmatric.realm")
-                //.encryptionKey(getKey())
-                .schemaVersion(1)
-                //.modules(new MySchemaModule())
-                //.migration(new MyMigration())
-                .build();
+//        Stetho.initialize(
+//                Stetho.newInitializerBuilder(this)
+//                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+//                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+//                        .build());
+//        RealmConfiguration config = new RealmConfiguration.Builder()
+//                .name("landmatric.realm")
+//                //.encryptionKey(getKey())
+//                .schemaVersion(1)
+//                //.modules(new MySchemaModule())
+//                //.migration(new MyMigration())
+//                .build();
+//        Realm.setDefaultConfiguration(config);
+
+        //method to generate realm db in external storage
+        initializeRealm();
+    }
+
+    private void initializeRealm() {
+
+        Realm.init(this);
+
+        RealmConfiguration.Builder realmBuilder = new RealmConfiguration.Builder();
+        realmBuilder.directory(new File(getRealmPath(this)));
+        realmBuilder.name("realmDataBase.realm");
+
+        RealmConfiguration config = realmBuilder.build();
         Realm.setDefaultConfiguration(config);
     }
 
+    public String getRealmPath(Context context) {
+
+        String externalStorageDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String packageName = context.getApplicationContext().getPackageName();
+        return externalStorageDir + File.separator + "RealmDemo" + File.separator;
+    }
 }
